525445f5a63d1b553f3ebac0ad519667d54c7ba9
LavishSharma05 <lavish.crick2005@gmail.com>
2025-10-05 18:44:10 +0530

    Local Storage

--- src/store/useHistory.js
+++ src/store/useHistory.js
@@ -1,21 +1,30 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useHistoryStore = create((set) => ({
-  history: [],
+const useHistoryStore = create(
+  persist(
+    (set) => ({
+      history: [],
 
-  setHistory: (items) => set({ history: items }),
+      setHistory: (items) => set({ history: items }),
 
-  addHistory: (item) =>
-    set((state) => ({
-      history: [item, ...state.history.slice(0, 49)],
-    })),
+      addHistory: (item) =>
+        set((state) => ({
+          history: [item, ...state.history.slice(0, 49)],
+        })),
 
-  deleteHistory: (id) =>
-    set((state) => ({
-      history: state.history.filter((item) => item.id !== id),
-    })),
+      deleteHistory: (id) =>
+        set((state) => ({
+          history: state.history.filter((item) => item.id !== id),
+        })),
 
-  clearHistory: () => set({ history: [] }),
-}));
+      clearHistory: () => set({ history: [] }),
+    }),
+    {
+      name: "history-storage", 
+      getStorage: () => localStorage, 
+    }
+  )
+);
 
 export default useHistoryStore;
